674934d9553415bc664c04e8515d3ec4a26feebc
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-14 20:16:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,9 @@ def monopoly_with_third_tier
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned => {},
+        :two_pieces_owned => {},
+        :three_pieces_owned => {},
+        :four_pieces_owned => {}
         
       }
     }
